8fe1e6b1f903225480605fd895aa830d433a1601
Christopher P <christopher.pfister@iqm.de>
2019-03-09 15:57:18 +0100

    Implemented object orientation / class fish
    A fish has the following attributes:
    * x-coordinate
    * y-coordinate
    * a direction (ltr)
    * name
    * speed
    
    A fish can:
    * move (means redraw)
    
    A fish's start coodinates are set in the constructor upon fish's creation

--- c-world.cpp
+++ c-world.cpp
@@ -23,43 +23,72 @@ void DrawObject(int x, int y, int rows, string content[], int color) {
 	}
 }
 
+class Fish {
+public:
+	// Member variables
+	string name = "Fish";
+	int x, y, speed = 1;
+	bool ltr = true;
+
+	// Constructor
+	Fish (int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	// Methods
+	void move(string fish_ltr[], string fish_rtl[]) {
+		for (int i = 0; i < speed; i++) {
+			if (ltr) {
+				DrawObject(x, y, 3, fish_ltr, 28);
+				cout << name << " ";
+				x++;
+			}
+			else {
+				DrawObject(x, y, 3, fish_rtl, 26);
+				cout << name << " ";
+				x--;
+			}
+
+			if (x > 80) ltr = false;
+			else if (x < 1) ltr = true;
+		}
+	}
+};
+
 int main() {
 	// Set up
 	system("color 1F");
-	int startX = 74;
-	bool ltr = true;
+	srand((unsigned)time(NULL)); // Randomness
+	int speed = 10 + rand() % (500 + 1) - 10;
 
-	// Randomness
-	srand((unsigned)time(NULL));
-	int random = 10 + rand() % (500 + 1) - 10;
-
-	// Load fish
+	// Fish design
 	string testfish_rtl[] = {
 		" o   . -= -.   ",
 		"  o (       >< ",
-		"     `- = -'   "};
-
+		"     `- = -'   " };
 	string testfish_ltr[] = {
 		"   . -= -.   o ",
 		" ><       ) o  ",
 		"   `- = -'     " };
 
+	// Load fish
+	Fish *maike = new Fish(74, 3);
+	maike->name = "Maike";
+
+	Fish* anna = new Fish(24, 20);
+	anna->name = "Anna";
+
+	Fish* christopher = new Fish(24, 12);
+	christopher->name = "Christopher";
+	christopher->speed = 2;
+
 	// Runtime loop
 	while (true) {
-		if (ltr) {
-			DrawObject(startX, 3, 3, testfish_ltr, 29);
-			startX++;
-		} else {
-			DrawObject(startX, 3, 3, testfish_rtl, 26);
-			startX--;
-		}
-		
-		if (startX > 80) ltr = false;
-		else if (startX < 1) ltr = true;
-
-		
-		cout << "Maike - " << random << "  ";
-		this_thread::sleep_for(chrono::milliseconds(random));
+		maike->move(testfish_ltr, testfish_rtl);
+		anna->move(testfish_ltr, testfish_rtl);
+		christopher->move(testfish_ltr, testfish_rtl);
+		this_thread::sleep_for(chrono::milliseconds(speed));
 	}
 
 	return 0;
