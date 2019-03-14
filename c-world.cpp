a49fdfc3c1613163a74b259712c628ffc6b6177c
Pfister <christopher.pfister@iqm.de>
2019-03-14 13:00:05 +0100

    Work on fish collisions and fish dying

--- c-world.cpp
+++ c-world.cpp
@@ -22,6 +22,14 @@ void DrawObject(int x, int y, list<string> content, int color) {
 	}
 }
 
+void removeFish(list<Fish> fishlist, string name) {
+	for (auto& fish : fishlist) {
+		if (fish.name == name) {
+			fishlist.remove(fish);
+		}
+	}
+}
+
 class Fish {
 public:
 	// Member variables
@@ -69,9 +77,22 @@ public:
 	void turn() {
 		ltr = !ltr;
 	}
+	void checkCollision(list<Fish> *fishlist) { // TODO: speed
+		for (auto fish : *fishlist) {
+			if (this->x == fish.x && this->y == fish.y && this->name != fish.name) {
+				cout << "COLLISION";
+				this_thread::sleep_for(chrono::milliseconds(10000));
+
+				/*if (this->fish_ltr.size() > fish.fish_ltr.size())
+					fishlist->remove(fish);
+				else
+					fishlist->remove(*this);*/
+			}
+		}
+	}
 };
 
-void fill_randomly(list<Fish>* fishlist, list<string> testfish_ltr, list<string> testfish_rtl, int count = 5) {
+void fill_randomly(list<Fish> * fishlist, list<string> testfish_ltr, list<string> testfish_rtl, int count = 5) {
 	for (int i = 0; i <= count; i++)
 		fishlist->push_front(Fish(rand() % 80 + 1, rand() % 24 + 1, testfish_ltr, testfish_rtl, rand() % 5 + 1));
 }
@@ -96,14 +117,17 @@ int main() {
 	// Load fish
 	list<Fish> fishlist;
 	fishlist.push_front(Fish(74, 12, testfish_ltr, testfish_rtl, 1, "Maike", 29));
-	fishlist.push_front(Fish(24, 20, testfish_ltr, testfish_rtl, 2, "Anna", 26));
-	fill_randomly(&fishlist, testfish_ltr, testfish_rtl);
+	fishlist.push_front(Fish(24, 12, testfish_ltr, testfish_rtl, 1, "Anna", 26));
+	//fill_randomly(&fishlist, testfish_ltr, testfish_rtl);
 
 	// Runtime loop
 	while (true) {
-		for (auto& fish : fishlist) fish.move();
+		for (auto& fish : fishlist) {
+			fish.move();
+			fish.checkCollision(&fishlist);
+		}
 		this_thread::sleep_for(chrono::milliseconds(tick));
 	}
-	
+
 	return 0;
 }
